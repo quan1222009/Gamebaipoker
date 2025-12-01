3082edb8aafb9242e30b0dd5b9282d0a8e4ea6f8
quan1222009 <12467ygjdjebieeee@gmail.com>
2025-12-01 23:23:36 +0700

    Create User.js

---
+++ models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    id: { type: String, required: true, unique: true },
+    friends: [String],
+    stats: {
+        gamesPlayed: { type: Number, default: 0 },
+        gamesWon: { type: Number, default: 0 }
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
