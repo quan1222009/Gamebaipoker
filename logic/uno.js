bc132e5e2639e33727c51286c0c2aae388a3db8f
quan1222009 <12467ygjdjebieeee@gmail.com>
2025-12-01 23:54:08 +0700

    Create uno.js

---
+++ logic/uno.js
@@ -0,0 +1,11 @@
+// Logic game Uno + bot
+function validMove(playerHand, tableCards, move){
+    // Kiểm tra màu, số, skip, reverse, draw2, wild...
+    return true;
+}
+
+function botMove(playerHand, tableCards){
+    return [playerHand[0]];
+}
+
+module.exports = { validMove, botMove };
