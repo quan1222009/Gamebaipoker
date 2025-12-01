97184a5cf8dbda748805ed947fd9ba728e96cfa0
quan1222009 <12467ygjdjebieeee@gmail.com>
2025-12-01 23:54:27 +0700

    Create cheat.js

---
+++ logic/cheat.js
@@ -0,0 +1,12 @@
+// Logic game Nói Dối + bot
+function validMove(playerHand, tableCards, move){
+    // Kiểm tra hợp lệ + bluff
+    return true;
+}
+
+function botMove(playerHand, tableCards){
+    // Bot có thể bluff
+    return [playerHand[0]];
+}
+
+module.exports = { validMove, botMove };
