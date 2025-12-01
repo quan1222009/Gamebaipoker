1ce4a9bca50cdfc43a696a0ea4b9c6a9ab9c86cb
quan1222009 <12467ygjdjebieeee@gmail.com>
2025-12-01 23:57:13 +0700

    Update sam.js

--- logic/sam.js
+++ logic/sam.js
@@ -1,13 +1,10 @@
-// Logic game Sâm + bot thông minh
 function validMove(playerHand, tableCards, move){
     // Kiểm tra combo hợp lệ: sảnh, đôi, ba, tứ quý, chặt heo
-    // Ví dụ prototype: luôn true
     return true;
 }
 
 function botMove(playerHand, tableCards){
-    // Bot đánh bài thông minh: chọn lá nhỏ nhất hoặc combo mạnh
-    return [playerHand[0]]; // prototype đánh lá đầu
+    return [playerHand[0]]; // Bot đánh lá đầu prototype
 }
 
 module.exports = { validMove, botMove };
