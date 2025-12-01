fe73aea67ac115ac2c622ed5e15c4f1559a9b0b8
quan1222009 <12467ygjdjebieeee@gmail.com>
2025-12-01 23:58:39 +0700

    Update tienlen.js

--- logic/tienlen.js
+++ logic/tienlen.js
@@ -1,11 +1,9 @@
-// Logic game Tiến Lên + bot
 function validMove(playerHand, tableCards, move){
-    // Kiểm tra sảnh, đôi, chặt heo, triple...
     return true;
 }
 
 function botMove(playerHand, tableCards){
-    return [playerHand[0]]; // đánh lá đầu prototype
+    return [playerHand[0]];
 }
 
 module.exports = { validMove, botMove };
